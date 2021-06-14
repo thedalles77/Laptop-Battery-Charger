27c7455c56be25a87437b9d8b734d70336bff091
Frank Adams <33405607+thedalles77@users.noreply.github.com>
2021-06-14 15:12:52 -0700

    Delete application.h

--- Cairo Fuel Gauge/application.h
+++
@@ -1,8 +0,0 @@
-
-#define WINDOW_WIDTH    70               // proposed width of main window 
-#define WINDOW_HEIGHT   40               // proposed height of main window 
-#define WINDOW_NAME     "Battery SOC%"    // name of main window 
-#define ICON_NAME       "./bat_icon.png"      // path to icon for window
-
-#define TIME_INTERVAL   5000              // time interval for timer function in msec
-                                          // 0 means no timer function 
